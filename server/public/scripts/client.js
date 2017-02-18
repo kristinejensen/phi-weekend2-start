f55189ac85d24adfb17ca755bc3c7944601f2f6f
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-18 15:05:31 -0600

    Basic display functionality working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
 
-    // Upon page load, get the data from the server
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        // yay! we have data!
-        console.log('returned data from server: ', data);
-      }
-    });
+  // Upon page load, get the data from the server
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data){
+      // yay! we have data!
+      console.log('returned data from server: ', data);
+      // DO NOT CHANGE ANY CODE ABOVE THIS LINE
+      // note -- console.log(data.phirephiters[0]); will access object in array
+      // note -- console.log(data.phirephiters[0].name); will access property of object in array
+
+      updateDOM(data.phirephiters);
+
+    }
+  })
+
+
+
+
+
+  function updateDOM (dataArray){
+    $('#phiMemberName').append(dataArray[4].name);
+    $('#phiGitLink').append(dataArray[4].git_username);
+    $('#phiShoutout').append(dataArray[4].shoutout);
+  }
+
+
+
 });
